1b6eafefdf823c11b337f8d19ccab8c52b416ba9
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2024-11-29 15:11:34 +0100

    Enhance jump logic

--- assets/scritps/statemachine/states/PlayerFall.gd
+++ assets/scritps/statemachine/states/PlayerFall.gd
@@ -1,11 +1,23 @@
 extends State
 class_name PlayerFall
 
+@export var fall_duration : float
+@export var max_fall_velocity : float
+
+
+@onready var player_jump_state = $"../Jump"
+@onready var fall_gravity : float = (-2.0 * player_jump_state.jump_height) / (player_jump_state.jump_ascension_duration * fall_duration) * -1
+
+func get_gravity() -> float:
+	return fall_gravity
+
 func can_enter() -> bool:
 	return super() and !player_physics_body.is_on_floor()  and player_physics_body.velocity.y > 0
 	
 func physics_update(delta: float):
-	player_physics_body.velocity += player_physics_body.get_gravity() * delta
+	player_physics_body.velocity.y += get_gravity() * delta
+	if player_physics_body.velocity.y > max_fall_velocity:
+		player_physics_body.velocity.y = max_fall_velocity
 	
 	if player_physics_body.is_on_floor():
 		state_machine.transition_to("run")
--- assets/scritps/statemachine/states/PlayerJump.gd
+++ assets/scritps/statemachine/states/PlayerJump.gd
@@ -1,17 +1,24 @@
 extends State
 class_name PlayerJump
 
-@export var jump_initial_velocity = -600.0
+@export var jump_height : float
+@export var jump_ascension_duration : float
+
+@onready var jump_velocity : float = (2.0 * jump_height) / jump_ascension_duration * -1
+@onready var jump_gravity : float = (-2.0 * jump_height) / pow(jump_ascension_duration, 2) * -1
+
+func get_gravity() -> float:
+	return jump_gravity
 
 func can_enter() -> bool:
-	var isFloor = player_physics_body.is_on_floor()
-	return super() and  isFloor
+	return super() and player_physics_body.is_on_floor()
 	
 func enter():
 	super()
-	player_physics_body.velocity.y = jump_initial_velocity	
+	player_physics_body.velocity.y = jump_velocity	
 	
 func physics_update(delta: float):
+	player_physics_body.velocity.y  += get_gravity() * delta
 	if player_physics_body.velocity.y > 0 and !player_physics_body.is_on_floor():
 		state_machine.transition_to("fall")
 		
