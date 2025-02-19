d22e2c54fb451537d61f1c297eefadf70a8d3e8e
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2025-02-19 14:36:17 +0100

    Add sprite orientation depending on speed

--- assets/scritps/PlayerAnimationController.gd
+++ assets/scritps/PlayerAnimationController.gd
@@ -1,2 +1,11 @@
 extends AnimatedSprite2D
 class_name PlayerAnimationController
+
+@onready
+var player_physics_body = $"../../Character body"
+
+func _process(delta: float) -> void:
+	check_player_orientation()
+	
+func check_player_orientation():
+	scale.x = player_physics_body.velocity.x / abs(player_physics_body.velocity.x)
