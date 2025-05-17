db7ba392f621cb3fa4c16292d21670f0e6296c5b
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2025-05-17 20:51:28 +0200

    Add collider detection

--- assets/scritps/player/CornerInteraction.gd
+++ assets/scritps/player/CornerInteraction.gd
@@ -1,1 +1,14 @@
-extends Node
+extends Area2D
+class_name CornerInteraction
+
+func _ready():
+	body_entered.connect(on_body_entered)
+
+func on_body_entered(body) :
+	if body == self.get_parent() : return
+	print("detected body " + body.name)
+	#for child in body.get_children() :
+		#if !child is CollisionShape2D : return
+		#
+		#var collider = child.shape
+		#if collider is RectangleShape2D : 
